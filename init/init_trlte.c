a154e17531535af50b03bc14183a3416e1408de4
Keith Lynch <spleef79@gmail.com>
2014-12-27 12:51:59 -0600

    Update init_trlte.c

--- init/init_trlte.c
+++ init/init_trlte.c
@@ -1,6 +1,5 @@
 /*
    Copyright (c) 2013, The Linux Foundation. All rights reserved.
-
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -13,7 +12,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -54,13 +52,20 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     property_get("ro.bootloader", bootloader);
 
-    if (strstr(bootloader, "N910P")) {
+    if (strstr(bootloader, "N910R4")) {
+        /* trlteusc */
+        property_set("ro.build.fingerprint", "samsung/trlteusc/trlteusc:4.4.4/KTU84P/N910R4VPU1ANIE:user/release-keys");
+        property_set("ro.build.description", "trlteusc-user 4.4.4 KTU84P N910R4UVU1ANIH release-keys");
+        property_set("ro.product.model", "SM-N910R4");
+        property_set("ro.product.device", "trlteusc");
+        cdma_properties("0", "311580", "U.S.Cellular");
+    } else {
         /* trltespr */
-        property_set("ro.build.fingerprint", "samsung/trltespr/trltespr:4.4.4/KTU84P/N910PVPU1ANK2:user/release-keys");
-        property_set("ro.build.description", "trltespr-user 4.4.4 KTU84P N910PVPU1ANK2 release-keys");
+        property_set("ro.build.fingerprint", "samsung/trltespr/trltespr:4.4.4/KTU84P/N910PVPU1ANIE:user/release-keys");
+        property_set("ro.build.description", "trltespr-user 4.4.4 KTU84P N910PVPU1ANIE release-keys");
         property_set("ro.product.model", "SM-N910P");
         property_set("ro.product.device", "trltespr");
-        cdma_properties("0", "310000", "Sprint");
+        cdma_properties("1", "310120", "Sprint");
     }
 
     property_get("ro.product.device", device);
@@ -68,14 +73,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     INFO("Found bootloader id %s setting build properties for %s device\n", bootloader, devicename);
 }
 
-void cdma_properties(char default_cdma_sub[], char operator_numeric[],
-        char operator_alpha[])
+void cdma_properties()
 {
-    property_set("ril.subscription.types", "NV,RUIM");
-    property_set("ro.cdma.home.operator.numeric", operator_numeric);
-    property_set("ro.cdma.home.operator.alpha", operator_alpha);
-    property_set("ro.telephony.default_cdma_sub", default_cdma_sub);
-    property_set("ro.telephony.default_network", "10");
-    property_set("ro.telephony.ril.config", "newDriverCallU,newDialCode");
     property_set("telephony.lteOnCdmaDevice", "1");
+    property_set("ro.telephony.default_network", "10");
+    property_set("ril.subscription.types", "NV,RUIM");
 }
