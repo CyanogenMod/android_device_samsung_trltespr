8e375a4cfcdba364882c38b3a9f5ff0050ed127b
Michael Wilkinson <mwilkinson@cmstactical.net>
2014-12-30 19:08:54 -0500

    Preperation for proper RIL

--- init/init_trlte.c
+++ init/init_trlte.c
@@ -73,9 +73,12 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
     INFO("Found bootloader id %s setting build properties for %s device\n", bootloader, devicename);
 }
 
-void cdma_properties()
+void cdma_properties(char cdma_sub[], char op_numeric[], char op_alpha[])
 {
-    property_set("telephony.lteOnCdmaDevice", "1");
-    property_set("ro.telephony.default_network", "10");
     property_set("ril.subscription.types", "NV,RUIM");
+    property_set("ro.cdma.home.operator.numeric", op_numeric);
+    property_set("ro.cdma.home.operator.alpha", op_alpha);
+    property_set("ro.telephony.default_cdma_sub", cdma_sub);
+    property_set("ro.telephony.default_network", "10");
+    property_set("telephony.lteOnCdmaDevice", "1");
 }
