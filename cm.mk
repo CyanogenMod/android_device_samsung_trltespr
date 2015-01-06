107947dc283c1931ae05cb723612274eac34d84f
Michael Wilkinson <mwilkinson@cmstactical.net>
2015-01-05 23:16:17 -0500

    one more try fixing derp

--- cm.mk
+++ cm.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/trltespr/full_trltespr.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit common LIQUID phone.
-$(call inherit-product, vendor/liquid/config/common_phone.mk)
+# Inherit common CM phone.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltespr
-PRODUCT_NAME := liquid_trltespr
+PRODUCT_NAME := cm_trltespr
