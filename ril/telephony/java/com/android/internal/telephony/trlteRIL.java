34bf4b5f6cf566055613e923e2334e788675d3e2
Michael Wilkinson <mwilkinson@cmstactical.net>
2015-01-06 11:21:05 -0500

    cleanup ril / network mode to LTE/CDMA by default

--- ril/telephony/java/com/android/internal/telephony/trlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/trlteRIL.java
@@ -240,43 +240,6 @@ public class trlteRIL extends RIL {
         send(rr);
     }
  
-    // Hack for Lollipop
-    // The system now queries for SIM status before radio on, resulting
-    // in getting an APPSTATE_DETECTED state. The RIL does not send an
-    // RIL_UNSOL_RESPONSE_SIM_STATUS_CHANGED message after the SIM is
-    // initialized, so delay the message until the radio is on.
-    @Override
-    public void
-    getIccCardStatus(Message result) {
-        if (mState != RadioState.RADIO_ON) {
-            mPendingGetSimStatus = result;
-        } else {
-            super.getIccCardStatus(result);
-        }
-    }
- 
-    @Override
-    protected void switchToRadioState(RadioState newState) {
-        super.switchToRadioState(newState);
- 
-        if (newState == RadioState.RADIO_ON && mPendingGetSimStatus != null) {
-            super.getIccCardStatus(mPendingGetSimStatus);
-            mPendingGetSimStatus = null;
-        }
-    }
-
-    @Override
-    public void
-    acceptCall (Message result) {
-        RILRequest rr
-                = RILRequest.obtain(RIL_REQUEST_ANSWER, result);
-
-        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
-		rr.mParcel.writeInt(1);
-		rr.mParcel.writeInt(0);
-        send(rr);
-    }
-
     @Override
     public void
     sendCdmaSms(byte[] pdu, Message result) {
@@ -511,6 +474,18 @@ public class trlteRIL extends RIL {
                 super.processUnsolicited(p);
                 return;
         }
+    }
+
+    @Override
+    public void
+    acceptCall (Message result) {
+        RILRequest rr
+                = RILRequest.obtain(RIL_REQUEST_ANSWER, result);
+
+        if (RILJ_LOGD) riljLog(rr.serialString() + "> " + requestToString(rr.mRequest));
+		rr.mParcel.writeInt(1);
+		rr.mParcel.writeInt(0);
+        send(rr);
     }
 
 }
