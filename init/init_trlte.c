219b120f3d361ea96c92789d08a623bca2c36f03
Michael Wawrzyniak <nivron@gmail.com>
2015-08-13 13:11:35 -0400

    trltespr: update fingerprint
    Change-Id: I534084d69639b97e75e6cd8e95f7e827b16a1fd9

--- init/init_trlte.c
+++ init/init_trlte.c
@@ -61,8 +61,8 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         cdma_properties("0", "311580", "U.S.Cellular");
     } else {
         /* trltespr */
-        property_set("ro.build.fingerprint", "samsung/trltespr/trltespr:5.0.1/LRX22C/N910PVPU1BOB7:user/release-keys");
-        property_set("ro.build.description", "trltespr-user 5.0.1 LRX22C N910PVPU1BOB7 release-keys");
+        property_set("ro.build.fingerprint", "samsung/trltespr/trltespr:5.1.1/LMY47X/N910PVPU4COG5:user/release-keys");
+        property_set("ro.build.description", "trltespr-user 5.1.1 LMY47X N910PVPU4COG5 release-keys");
         property_set("ro.product.model", "SM-N910P");
         property_set("ro.product.device", "trltespr");
         cdma_properties("1", "310120", "Sprint");
